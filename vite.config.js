2f453d5f299d5edfcc44040ae01ec2ce0b546e26
Constantine Tereschenkov <kdubrovsky@gmail.com>
2023-05-14 16:11:25 +0400

    Add svg spritemap vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { svgSpritemap } from 'vite-plugin-svg-spritemap';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        svgSpritemap({
+            pattern: 'src/assets/icons/**/*.svg',
+            prefix: 'icon',
+        })
+    ],
 })
+
