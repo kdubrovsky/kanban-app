a42f8083131ee503050f3be375e6d640b6e80a0b
Constantine Tereschenkov <kdubrovsky@gmail.com>
2023-05-14 21:21:17 +0400

    Rename and move icons, refactor Icon component

--- src/components/ui/Icon/Icon.jsx
+++ src/components/ui/Icon/Icon.jsx
@@ -1,9 +1,9 @@
 function Icon({
-    name,
+    name, size = 16
 }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-            <use xlinkHref={`/spritemap.svg#icon-${name}`} />
+            <use xlinkHref={`/spritemap.svg#icon-${name}-${size}px`} />
         </svg>
     )
 }
